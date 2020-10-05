76aedd71a524b9c7bd816747bd8774344fc77a40
devarshishah3 <devarshishah3@gmail.com>
2020-10-04 19:49:11 -0700

    removed address groups

--- main.tf
+++ main.tf
@@ -22,31 +22,10 @@ resource "panos_dag_tags" "example" {
   }
 }
 
-resource "panos_address_group" "addresses_group" {
-  # https://docs.paloaltonetworks.com/pan-os/8-1/pan-os-web-interface-help/objects/objects-address-groups.html
-
-  for_each      = local.service_names
-  # Cleanse the service NAME to meet character constraints for addr obj names
-  name          = replace("${var.dag_prefix}${each.key}${var.dag_suffix}", "/[^0-9A-Za-z]/", "-")
-  # create a match condition based on service name registered in Consul 
-  description   = "Dynamic address group generated for service ${each.key} registered in Consul"
-  # Add dynamic match based on service name only
-  dynamic_match = "'${each.key}'"
-}  
-
 
 locals {
-  # Creating a map with key as service name instead of service id
-  service_names = transpose({
-    for id, s in var.services : id => [s.name]
-  })
   # Creating a map for tag to ip address mapping  
   tags_association = {
     for id, s in var.services : s.name => [ (s.address == "" ? s.node_address : s.address)]...
   }
-  #Creating a map for address group objects created
-  address_group_names = transpose({
-    for id, s in var.services : id => [replace("${var.dag_prefix}${s.name}${var.dag_suffix}", "/[^0-9A-Za-z]/", "-")]
-  })
-
 }
