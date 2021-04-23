fe178586e8e356abf8100905c44dd25fc0157c66
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2021-04-23 13:16:03 +0100

    update module to use new panos_ip_tag resource

--- main.tf
+++ main.tf
@@ -2,30 +2,25 @@ terraform {
   required_providers {
     panos = {
       source  = "paloaltonetworks/panos"
-      version = "~>1.6"
+      version = "~>1.8.3"
     }
   }
 }
 
-
-resource "panos_dag_tags" "example" {
+resource "panos_ip_tag" "this" {
   # https://docs.paloaltonetworks.com/pan-os/9-1/pan-os-admin/policy/register-ip-addresses-and-tags-dynamically
-
   for_each = var.services
-  # vsys name to associate IP address and TAG 
-  vsys     = var.vsys_name
-  register {    
-      # Service or node addess to associate the TAG 
-      ip   = each.value.address == "" ? each.value.node_address : each.value.address
-      # TAGs based on service name
-      tags =  [each.value.name]
-  }
-}
 
+  # Service or node addess to associate the TAG 
+  ip = each.value.address == "" ? each.value.node_address : each.value.address
+
+  # TAGs based on service name
+  tags = [each.value.name]
+}
 
 locals {
   # Creating a map for tag to ip address mapping  
   tags_association = {
-    for id, s in var.services : s.name => [ (s.address == "" ? s.node_address : s.address)]...
+    for id, s in var.services : s.name => [(s.address == "" ? s.node_address : s.address)]...
   }
 }
